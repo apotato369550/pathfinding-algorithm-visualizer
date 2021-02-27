3de481dd8c15ed395bbf430eeac2cca304b87e7b
apotato369550 <johnandresyap510@gmail.com>
2021-02-27 19:16:40 +0800

    Add main file

---
+++ main.py
@@ -0,0 +1,9 @@
+
+import pygame
+import math
+from queue import PriorityQueue
+
+LENGTH = 1000
+WIN = pygame.display.set_mode((LENGTH, LENGTH))
+pygame.display.set_caption("A* Path Finding Visualizer")
+
