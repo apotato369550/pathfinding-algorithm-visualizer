ac8921ee4fdae106695f7fe3795ef9794161caab
apotato369550 <johnandresyap510@gmail.com>
2021-03-01 10:44:07 +0800

    Finish Cell Class and Add Heuristic function

--- main.py
+++ main.py
@@ -50,4 +50,30 @@ class Cell:
     def reset(self):
         self.color == WHITE
 
+    def make_closed(self):
+        self.color = RED
+
+    def make_open(self):
+        self.color = GREEN
+
+    def make_barrier(self):
+        self.color = BLACK
+
+    def make_end(self):
+        self.color = TURQUOISE
+
+    def make_path(self):
+        self.color = PURPLE
+
+    def draw(self, window):
+        pygame.draw.rect(window, self.color, (self.x, self.y, self.length, self.length))
     # continue here
+
+    def update_neighbors(self, grid):
+        return
+
+    def __lt__(self, other):
+        return False
+
+def heuristic(point_1, point_2):
+    return
